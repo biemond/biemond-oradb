0990788721048fde08d4301de4dee88c04e03972
Stanisław Chlipała <stanislaw.chlipala@coi.gov.pl>
2014-02-15 10:31:10 +0100

    Fix - file oradb::database & dbca_11.2.rsp.erb. When install Oracle database 11.2.0.1 Oracle reported: ORA-00401: the value for parameter compatible is not supported by this release. Parameter: EMCONFIGURATION.
    Oracle recommended:
    ORA-00401: the value for parameter string is not supported by this release
        Cause: The value specified cannot be supported by this release of the software.
        Action: Choose an appropriate value, or remove the parameter value to use the default value.

--- manifests/database.pp
+++ manifests/database.pp
@@ -8,6 +8,8 @@
 #  oradb::database{ 'testDb':
 #                   oracleBase              => '/oracle',
 #                   oracleHome              => '/oracle/product/11.2/db',
+#                   version                 => "11.2",
+#                   fullVersion             => "11.2.0.1", 
 #                   user                    => 'oracle',
 #                   group                   => 'dba',
 #                   downloadDir             => '/install',
@@ -25,6 +27,7 @@
 #                   memoryPercentage        => "40",
 #                   memoryTotal             => "800",
 #                   databaseType            => "MULTIPURPOSE",
+#                   emConfiguration         => "NONE", 
 #                   require                 => Oradb::Listener['start listener'],
 #  }
 #
@@ -33,6 +36,7 @@
 define oradb::database( $oracleBase               = undef,
                         $oracleHome               = undef,
                         $version                  = "11.2",
+						$fullVersion              = "11.2.0.1",
                         $user                     = 'oracle',
                         $group                    = 'dba',
                         $downloadDir              = '/install',
@@ -50,6 +54,7 @@ define oradb::database( $oracleBase               = undef,
                         $memoryPercentage         = "40",
                         $memoryTotal              = "800",
                         $databaseType             = "MULTIPURPOSE",
+						$emConfiguration          = "NONE",
 )
 
 {
@@ -67,7 +72,7 @@ define oradb::database( $oracleBase               = undef,
         Exec { path  => $execPath,
           user       => $user,
           group      => $group,
-		  environment => [
+          environment => [
               "USER=${user}",
           ],
           logoutput  => true,
