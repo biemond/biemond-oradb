33ea4dddf4c16b5f0a85ed1cb0fe6b70af7feea7
Edwin Biemond <biemond@gmail.com>
2014-07-23 14:30:42 +0200

    coverage support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,24 @@
-require 'rubygems'
+begin
+  require 'coveralls'
+  Coveralls.wear!
+
+rescue LoadError
+  puts "No Coveralls support"
+end
+
+require 'rspec-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+# include common helpers
+support_path = File.expand_path(File.join(File.dirname(__FILE__), '..','spec/support/*.rb'))
+Dir[support_path].each {|f| require f}
+
+RSpec.configure do |c|
+  c.config = '/doesnotexist'
+  c.module_path  = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/modules'))
+  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
+end
+
+def param_value(subject, type, title, param)
+  subject.resource(type, title).send(:parameters)[param.to_sym]
+end
