114e2309b828cb00fde36ef623860d2ae9078ae5
Edwin Biemond <biemond@gmail.com>
2014-08-06 20:03:16 +0200

    new util manifests

---
+++ manifests/utils/dborainst.pp
@@ -0,0 +1,33 @@
+# == define: oradb::utils::dborainst
+#
+#  creates oraInst.loc for oracle products
+#
+#
+##
+define oradb::utils::dborainst
+(
+  $ora_inventory_dir = undef,
+  $os_group          = undef,
+){
+  case $::kernel {
+    'Linux': {
+      $oraInstPath = '/etc'
+    }
+    'SunOS': {
+      $oraInstPath = '/var/opt/oracle'
+      if !defined(File[$oraInstPath]) {
+        file { $oraInstPath:
+          ensure  => directory,
+          before  => File["${oraInstPath}/oraInst.loc"],
+        }
+      }
+    }
+  }
+
+  if !defined(File["${oraInstPath}/oraInst.loc"]) {
+    file { "${oraInstPath}/oraInst.loc":
+      ensure  => present,
+      content => template('oradb/oraInst.loc.erb'),
+    }
+  }
+}
