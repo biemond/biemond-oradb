554d336f3e8cb4835be04cc47dc501f5657db5dc
Edwin Biemond <biemond@gmail.com>
2018-02-16 22:09:50 +0100

    fix for all puppet versions

--- spec/defines/database_params_spec.rb
+++ spec/defines/database_params_spec.rb
@@ -166,7 +166,7 @@ describe 'oradb::database', :type => :define do
 
       it "should pass" do
         expect { should contain_file("/install/database_testDb_Create.rsp")
-                 }.to raise_error(Puppet::Error, /expects a value of type Undef or Hash/)
+                 }.to raise_error(Puppet::Error, /Hash/)
       end
 
     end
@@ -187,7 +187,7 @@ describe 'oradb::database', :type => :define do
 
       it "should fail" do
         expect { should contain_file("/install/database_testDb_Create.rsp")
-                 }.to raise_error(Puppet::Error, /expects a value of type Undef or Hash/)
+                 }.to raise_error(Puppet::Error, /Hash/)
       end
 
     end
